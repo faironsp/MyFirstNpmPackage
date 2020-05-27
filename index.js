bcf12893e658c8746adc20114de9318baa782f86
Fabio <faironsp@gmail.com>
2020-05-26 21:30:23 -0300

    criação de funções

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.NumeroPar = function (n) {
+    var resposta = false;
+    
+    if (n % 2 == 0) {
+        resposta = true;
+    }
+
+    return resposta;
+}
+
+exports.NumeroPrimo = function (n) {
+    var retorno = false;
+    
+    //dividir o numero n vezes
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count = count + 1;
+        }
+    }
+
+    if (count == 2) {
+        retorno = true;
+    }
+
+    return retorno;
+}
